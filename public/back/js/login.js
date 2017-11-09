c1d519e0554d6b74efa55d3292a7660b55763a15
Your Name <1124215161@qq.com>
2017-11-09 11:11:19 +0800

    使用了bootsrapValitor插件

--- public/back/js/login.js
+++ public/back/js/login.js
@@ -25,19 +25,20 @@ $(function() {
 
         //3. 指定校验字段
         fields: {
-            //校验用户名，对应name表单的name属性
+            //3.1校验用户名，对应name表单的name属性
             username: {
+                //3.2在这个对象中写所有的校验规则
                 validators: {
-                    //不能为空
+                    //不能为空(在输入框下所要提示的内容)
                     notEmpty: {
                         message: '用户名不能为空'
                     },
-                    //长度校验
-                    stringLength: {
-                        min: 6,
-                        max: 12,
-                        message: '用户名长度必须在6到30之间'
-                    },
+                    ////长度校验
+                    //stringLength: {
+                    //    min: 6,
+                    //    max: 12,
+                    //    message: '用户名长度必须在6到30之间'
+                    //},
                     //正则校验
                     regexp: {
                         regexp: /^[a-zA-Z0-9_\.]+$/,
@@ -45,13 +46,29 @@ $(function() {
                     }
                 }
             },
+
+            //3.3校验密码(对应name表单中的name属性名)
+            password:{
+                //3.4在这个密码表单中的方法下写所有的校验规则
+                validators:{
+                    //3.5校验密码不能为空
+                    notEmpty:{
+                        message:"密码不能为空"
+                    },
+                    //3.6校验密码长度为6-12位
+                    stringLength:{
+                        min:6,
+                        max:12,
+                        message:"密码长度是6-12位"
+                    }
+                }
+            }
         }
     });
-    //3.注册表单验证成功
-    $form.on('success.form.bv', function (e) {
-        e.preventDefault();
-        //使用ajax提交逻辑
-    });
+
+    //4.为了校验成功之后,不让表单自动提交过去,那么,可以在校验成功之后进行点击,防止提交表单(禁用);同时,
+    //可以在校验成功之后,触发一个事件
+
 })
 
 
